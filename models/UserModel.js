73b25bf26a8d9be7bf998bd490c5983b4a5cb653
Armin Hupka <arminhupka@me.com>
2021-06-30 17:45:01 +0200

    - added errorMiddleware.js

---
+++ models/UserModel.js
@@ -0,0 +1,46 @@
+import mongoose from 'mongoose';
+
+const UserModel = mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    firstName: {
+      type: String,
+      required: true,
+    },
+    lastName: {
+      type: String,
+      required: true,
+    },
+    pesel: {
+      type: Number,
+      required: true,
+    },
+    street: {
+      type: String,
+      required: true,
+    },
+    city: {
+      type: String,
+      required: true,
+    },
+    postalCode: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    versionKey: false,
+  },
+);
+
+const User = mongoose.model('User', UserModel);
+
+export default User;
