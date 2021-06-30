de77dfd4066a8bde1c0fd0a8a20853ad541e6891
Armin Hupka <arminhupka@me.com>
2021-06-30 16:36:09 +0200

    - added eslint & prettier

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
-import express from 'express'
+import express from "express";
+import dotenv from "dotenv";
 
-const app = express()
+dotenv.config();
 
-app.get("/", (req, res) => res.send('Hello World'));
+const app = express();
 
-const PORT = process.env.PORT || 5000
+app.get("/", (req, res) => res.send("Hello World"));
 
-app.listen(PORT, () => {console.log(`Server started on port ${PORT}`)})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
