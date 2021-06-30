770b7f4a58837d0959c0d01a4ba4c0082a04f4cf
Armin Hupka <arminhupka@me.com>
2021-06-30 18:02:07 +0200

    - added userLogin - created jwtGen.js

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,12 +1,14 @@
 import asyncHandler from 'express-async-handler';
+import bcrypt from 'bcrypt';
 import User from '../models/UserModel.js';
+import jwtGen from '../lib/jwtGen.js';
 
 export const userRegister = asyncHandler(async (req, res) => {
   const { email, password, firstName, lastName, pesel, street, city, postalCode } = req.body;
 
   const user = await User.create({
     email,
-    password,
+    password: await bcrypt.hash(password, 10),
     firstName,
     lastName,
     pesel,
@@ -15,14 +17,35 @@ export const userRegister = asyncHandler(async (req, res) => {
     postalCode,
   });
 
-  if (user) {
-    const { password, ...responseUser } = user._doc;
-    res.status(201).json(responseUser);
-  }
+  res.status(201).json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    token: jwtGen(user._id),
+  });
 });
 
 export const userLogin = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
 
-  res.send('login');
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404);
+    throw new Error('Email or password not correct');
+  }
+
+  const matchPassword = await bcrypt.compare(password, user.password);
+
+  if (!matchPassword) {
+    res.status(404);
+    throw new Error('Email or password not correct');
+  }
+
+  res.json({
+    firstName: user.firstName,
+    lastName: user.lastName,
+    email: user.email,
+    token: jwtGen(user._id),
+  });
 });
